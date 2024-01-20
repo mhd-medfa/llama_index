1842f1e8ace5d980b98d6fac5fb0d35c6cb5599f
Mohamad Al Mdfaa <mohamad.almdfaa@gmail.com>
2024-01-20 16:58:24 +0300

    Update sentence_transformer.py

--- llama_index/finetuning/embeddings/sentence_transformer.py
+++ llama_index/finetuning/embeddings/sentence_transformer.py
@@ -10,6 +10,15 @@ from llama_index.finetuning.embeddings.common import (
 from llama_index.finetuning.types import BaseEmbeddingFinetuneEngine
 
 
+from sentence_transformers import InputExample, SentenceTransformer, losses
+from torch.utils.data import DataLoader
+from sentence_transformers.evaluation import InformationRetrievalEvaluator
+from transformers import TrainingArguments, Trainer
+from transformers.optimization import get_linear_schedule_with_warmup
+import torch
+import os
+from typing import Optional, Any
+
 class SentenceTransformersFinetuneEngine(BaseEmbeddingFinetuneEngine):
     """Sentence Transformers Finetune Engine."""
 
@@ -26,51 +35,30 @@ class SentenceTransformersFinetuneEngine(BaseEmbeddingFinetuneEngine):
         evaluation_steps: int = 50,
     ) -> None:
         """Init params."""
-        from sentence_transformers import InputExample, SentenceTransformer, losses
-        from torch.utils.data import DataLoader
-        import torch.nn as nn
-        import torch
-        # Check the number of available GPUs
-        num_gpus = torch.cuda.device_count()
-        
-        # Specify the devices to be used (cuda:0, cuda:1, ...)
-        devices = [torch.cuda.device(f'cuda:{i}') for i in range(num_gpus)]
-
         self.dataset = dataset
-
         self.model_id = model_id
         self.model_output_path = model_output_path
+
         self.model = SentenceTransformer(model_id)
         self.model = self.model.to('cuda:0')
 
         # Wrap the model with DataParallel to utilize multiple GPUs
-        self.model = torch.nn.DataParallel(self.model, device_ids=[device.idx for device in 
-        devices])
+        num_gpus = torch.cuda.device_count()
+        devices = [f'cuda:{i}' for i in range(num_gpus)]
+        self.model = torch.nn.DataParallel(self.model, device_ids=[device.idx for device in devices])
 
-        # TODO: support more than 1 doc per query
-        examples: Any = []
-        for query_id, query in dataset.queries.items():
-            node_id = dataset.relevant_docs[query_id][0]
-            text = dataset.corpus[node_id]
-            example = InputExample(texts=[query, text])
-            examples.append(example)
-        self.examples = examples
+        self.examples = [InputExample(texts=[query, dataset.corpus[dataset.relevant_docs[query_id][0]]])
+                         for query_id, query in dataset.queries.items()]
 
-        self.loader: DataLoader = DataLoader(examples, batch_size=batch_size)
+        self.loader = DataLoader(self.examples, batch_size=batch_size)
 
-        # define evaluator
-        from sentence_transformers.evaluation import InformationRetrievalEvaluator
-
-        evaluator: Optional[InformationRetrievalEvaluator] = None
+        self.evaluator = None
         if val_dataset is not None:
-            evaluator = InformationRetrievalEvaluator(
+            self.evaluator = InformationRetrievalEvaluator(
                 val_dataset.queries, val_dataset.corpus, val_dataset.relevant_docs
             )
-        self.evaluator = evaluator
 
-        # define loss
         self.loss = loss or losses.MultipleNegativesRankingLoss(self.model)
-
         self.epochs = epochs
         self.show_progress_bar = show_progress_bar
         self.evaluation_steps = evaluation_steps
@@ -78,16 +66,38 @@ class SentenceTransformersFinetuneEngine(BaseEmbeddingFinetuneEngine):
 
     def finetune(self, **train_kwargs: Any) -> None:
         """Finetune model."""
-        
-        self.model.fit(
-            train_objectives=[(self.loader, self.loss)],
-            epochs=self.epochs,
+        output_dir = self.model_output_path
+        os.makedirs(output_dir, exist_ok=True)
+
+        training_args = TrainingArguments(
+            output_dir=output_dir,
+            overwrite_output_dir=True,
+            num_train_epochs=self.epochs,
+            per_device_train_batch_size=train_kwargs.get('per_device_train_batch_size', 10),
+            save_steps=train_kwargs.get('save_steps', 100),
+            save_total_limit=train_kwargs.get('save_total_limit', 2),
+            evaluation_strategy=train_kwargs.get('evaluation_strategy', 'steps'),
+            eval_steps=self.evaluation_steps,
+            logging_dir=train_kwargs.get('logging_dir', os.path.join(output_dir, 'logs')),
+            logging_steps=train_kwargs.get('logging_steps', 100),
+            learning_rate=train_kwargs.get('learning_rate', 2e-5),
             warmup_steps=self.warmup_steps,
-            output_path=self.model_output_path,
-            show_progress_bar=self.show_progress_bar,
-            evaluator=self.evaluator,
-            evaluation_steps=self.evaluation_steps,
         )
+
+        trainer = Trainer(
+            model=self.model,
+            args=training_args,
+            train_dataset=self.loader,
+            data_collator=train_kwargs.get('data_collator', None),
+            compute_metrics=train_kwargs.get('compute_metrics', None),
+            callbacks=train_kwargs.get('callbacks', None),
+            tb_writer=train_kwargs.get('tb_writer', None),
+            optimizers=train_kwargs.get('optimizers', None),
+            scheduler=train_kwargs.get('scheduler', get_linear_schedule_with_warmup),
+        )
+
+        trainer.train()
+
 
     def get_finetuned_model(self, **model_kwargs: Any) -> BaseEmbedding:
         """Gets finetuned model."""
